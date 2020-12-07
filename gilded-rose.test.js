632a1f46ddacc6cabddd7975ab0cf241f663d3c3
Ricardo Borillo <borillo@gmail.com>
2020-12-07 18:21:25 +0100

    Kata con approval test basado en jest-extended-snapshot

---
+++ gilded-rose.test.js
@@ -0,0 +1,22 @@
+const { Item, Shop } = require("./gilded-rose");
+
+describe("Gilded Rose", () => {
+  it("should update quality", () => {
+    expect(doUpdateQuality).toVerifyAllCombinations(
+      [
+        "foo",
+        "Aged Brie",
+        "Backstage passes to a TAFKAL80ETC concert",
+        "Sulfuras, Hand of Ragnaros"
+      ],
+      [-1, 0, 2, 6, 10, 11],
+      [0, 1, 49, 50]
+    );
+  });
+
+  function doUpdateQuality(name, sellIn, quality) {
+    const gildedRose = new Shop([new Item(name, sellIn, quality)]);
+    const items = gildedRose.updateQuality();
+    return items[0];
+  }
+});
